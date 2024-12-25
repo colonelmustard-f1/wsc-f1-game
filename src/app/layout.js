6dbed017ff0c846fe519ce772d54c8bfa4f90e3c
colonelmustard-f1 <marioarjona7@gmail.com>
2024-12-24 23:28:10 -0600

    Create layout.js

---
+++ src/app/layout.js
@@ -0,0 +1,15 @@
+// src/app/layout.js
+import '@/styles/globals.css'
+
+export const metadata = {
+  title: 'WSC F1 Game',
+  description: 'World Selector\'s Championship F1 Prediction Game',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
